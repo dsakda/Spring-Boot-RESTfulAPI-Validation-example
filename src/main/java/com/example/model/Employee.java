37f2ebbe42f110b877bb51755f91ff149a53dea2
dsakda <74044765+dsakda@users.noreply.github.com>
2021-01-02 00:22:59 +0700

    Update Employee.java

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -23,27 +23,27 @@ public class Employee {
     private Long id;
 
     @NotBlank
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name")
     @Size(min = 2, message = "First Name should have atleast 2 characters")
     private String firstName;
 
     @NotBlank
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name")
     @Size(min = 2, message = "Last Name should have atleast 2 characters")
     private String lastName;
 
     @NotBlank
     @Email
-    @Column(name = "email_address", nullable = false)
+    @Column(name = "email_address")
     private String emailAddress;
 
     @NotBlank
-    @Column(name = "passport_number", nullable = false)
+    @Column(name = "passport_number")
     @Size(min = 2, message = "Passport should have atleast 2 characters")
     private String passportNumber;
 
     @NotNull
-    @Column(name = "birth_day", nullable = false)
+    @Column(name = "birth_day")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthDay;
 
